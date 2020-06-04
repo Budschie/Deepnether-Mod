c5377225a164845d7252cb0fd4b4b98df51612ad
Budschie <65289971+Budschie@users.noreply.github.com>
2020-06-04 16:44:34 +0200

    Fixed entity rendering crashes

--- src/main/java/de/budschie/deepnether/main/DeepnetherMain.java
+++ src/main/java/de/budschie/deepnether/main/DeepnetherMain.java
@@ -7,13 +7,18 @@ import org.apache.logging.log4j.Logger;
 
 import de.budschie.deepnether.biomes.BiomeFeatureAdder;
 import de.budschie.deepnether.block.BlockInit;
+import de.budschie.deepnether.capabilities.ToolDefinitionCapability;
 import de.budschie.deepnether.commands.CommandPlaceStructure;
+import de.budschie.deepnether.commands.CommandPlaceTree;
 import de.budschie.deepnether.commands.CommandSaveSelection;
 import de.budschie.deepnether.commands.CommandToggleSel;
 import de.budschie.deepnether.container.DeepNetherBlastFurnaceContainer;
 import de.budschie.deepnether.dimension.DimensionInit;
 import de.budschie.deepnether.entity.renders.EntityRenderInit;
 import de.budschie.deepnether.gui.NetherBlastFurnaceGUI;
+import de.budschie.deepnether.item.ItemInit;
+import de.budschie.deepnether.item.ToolUsableItemRegistry;
+import de.budschie.deepnether.item.recipes.ToolRecipeSerializerRegistry;
 import de.budschie.deepnether.networking.FogSeedMessageRecieve;
 import de.budschie.deepnether.networking.PullFogMessage;
 import de.budschie.deepnether.networking.StructureIDPacket;
@@ -25,11 +30,14 @@ import net.minecraft.block.IceBlock;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
@@ -64,6 +72,7 @@ public class DeepnetherMain
         
         Features.FEATURES.register(FMLJavaModLoadingContext.get().getModEventBus());
         Features.PLACEMENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ToolRecipeSerializerRegistry.DEF_REG_RECIPE.register(FMLJavaModLoadingContext.get().getModEventBus());
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -73,6 +82,8 @@ public class DeepnetherMain
     //preinit
     private void setup(final FMLCommonSetupEvent event)
     {
+    	ToolDefinitionCapability.register();
+
         // some preinit code
     	INSTANCE.<FogSeedMessageRecieve>registerMessage(disc++, FogSeedMessageRecieve.class, FogSeedMessageRecieve::encodeAtServer, FogSeedMessageRecieve::decodeAtClient, FogSeedMessageRecieve::handleAtClient);
     	INSTANCE.<PullFogMessage>registerMessage(disc++, PullFogMessage.class, PullFogMessage::pull, PullFogMessage::pulled, PullFogMessage::handleAtServer);
@@ -84,6 +95,8 @@ public class DeepnetherMain
     	StructureDataProviderRegistry.addEntriesFromRegistry();
     	
     	BiomeFeatureAdder.addFeatures();
+    	
+    	ToolUsableItemRegistry.init();
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) 
@@ -100,6 +113,9 @@ public class DeepnetherMain
     	RenderTypeLookup.setRenderLayer(BlockInit.ANCIENT_WITHERED_LEAVES, RenderType.getTranslucent());
     	
     	ScreenManager.registerFactory(DeepNetherBlastFurnaceContainer.TYPE, NetherBlastFurnaceGUI.FACTORY);
+    	
+    	EntityRenderInit.registerEntityRenders();   	
+    	
         // do something that can only be done on the client
         //LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
@@ -132,5 +148,6 @@ public class DeepnetherMain
     	CommandToggleSel.register(event.getCommandDispatcher());
     	CommandSaveSelection.registerCommandTpDim(event.getCommandDispatcher());
     	CommandPlaceStructure.registerCommandTpDim(event.getCommandDispatcher());
+    	CommandPlaceTree.register(event.getCommandDispatcher());
     }
 }
