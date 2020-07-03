5435891c35bb853740f76ddbb78c270e59a91954
Budschie <65289971+Budschie@users.noreply.github.com>
2020-07-03 09:01:08 +0200

    Add files via upload

--- src/main/java/de/budschie/deepnether/dimension/DeepnetherBiomeProvider.java
+++ src/main/java/de/budschie/deepnether/dimension/DeepnetherBiomeProvider.java
@@ -1,49 +1,53 @@
 package de.budschie.deepnether.dimension;
 
+import java.util.List;
+import java.util.Set;
+
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.mojang.serialization.Codec;
 
 import de.budschie.deepnether.biomes.BiomeRegistry;
+import de.budschie.deepnether.biomes.DeepnetherBiomeBase;
+import de.budschie.deepnether.biomes.FloatingIslandsBiome;
+import it.unimi.dsi.fastutil.longs.Long2IntLinkedOpenHashMap;
+import net.minecraft.block.BlockState;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraft.world.biome.provider.NetherBiomeProvider;
 import net.minecraft.world.biome.provider.OverworldBiomeProvider;
+import net.minecraft.world.gen.LazyAreaLayerContext;
 import net.minecraft.world.gen.area.IAreaFactory;
 import net.minecraft.world.gen.area.LazyArea;
 import net.minecraft.world.gen.layer.Layer;
 import net.minecraft.world.gen.layer.LayerUtil;
+import net.minecraft.world.gen.layer.traits.IPixelTransformer;
 
 public class DeepnetherBiomeProvider extends BiomeProvider
 {
-	private final Layer genBiomes;
-	
-	public DeepnetherBiomeProvider()
+
+	protected DeepnetherBiomeProvider(List<Biome> p_i231634_1_)
 	{
-		//OverworldBiomeProvider
-		super(ImmutableSet.<Biome>of(BiomeRegistry.DEEPNETHER_BIOME, BiomeRegistry.CRYSTAL_CAVE_BIOME, BiomeRegistry.FLOATING_ISLANDS_BIOME, BiomeRegistry.LAVA_BIOME, BiomeRegistry.SOULFUL_GRASS_BIOME));
-		this.genBiomes = new Layer(new IAreaFactory<LazyArea>()
-		{
-			
-			@Override
-			public LazyArea make()
-			{
-				IAreaFactory<LazyArea> factory = new IAreaFactory<LazyArea>()
-				{
-					
-					@Override
-					public LazyArea make()
-					{
-						LazyArea lazyArea = new LazyArea(p_i51286_1_, p_i51286_2_, p_i51286_3_);
-						return null;
-					}
-				};
-				
-				return factory.make();
-			}
-		});
+		super(p_i231634_1_);
 	}
 
 	@Override
 	public Biome getNoiseBiome(int x, int y, int z)
 	{
+		NetherBiomeProvider
 		return null;
 	}
+
+	@Override
+	protected Codec<? extends BiomeProvider> func_230319_a_()
+	{
+		return null;
+	}
+
+	@Override
+	public BiomeProvider func_230320_a_(long p_230320_1_)
+	{
+		return null;
+	}
+	
 }
